902b2037038bf92d9c8c11855dfc240181af0b51
oita-joho <oitajoho@gmail.com>
2026-04-12 15:50:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1217,7 +1217,7 @@ function hostView() {
       <div class="notice">
         設定係では、名簿の読込・編集・保存、開始グループの切替、トークン確認、QR印刷ができます。<br>
         必要な項目だけ開いて使ってください。<br>
-        手順0.事前にCSVを読み込む 1.fireに保存 2.当日fireから読み込みグループに保存 3.実施
+        手順0.事前にCSVを読み込む 1.fireに保存 2.当日fireから読み込みグループに保存 3.実施する
       </div>
       <div class="card">
   <div class="notice">
